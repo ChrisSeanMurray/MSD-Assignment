166a097e101e3888e45f68d1c845cd5dc081f4f5
Chris Murray <chris.sean.murray@gmail.com>
2016-11-27 17:42:27 +0000

    app can now successfully take pictures

--- app/src/main/java/ie/dit/msd_assignment/EditEntry.java
+++ app/src/main/java/ie/dit/msd_assignment/EditEntry.java
@@ -3,24 +3,32 @@ package ie.dit.msd_assignment;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class EditEntry extends AppCompatActivity implements View.OnClickListener{
+import java.text.SimpleDateFormat;
+
+public class EditEntry extends AppCompatActivity {
     JournalDBManager db;
     Bundle b;
     Cursor c;
     EditText arrows;
     EditText venue;
     EditText details;
-    Button update;
-    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_entry);
+
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar) ;
+        setSupportActionBar(myToolbar);
 
         db = new JournalDBManager(this);
         b = this.getIntent().getExtras();
@@ -32,27 +40,41 @@ public class EditEntry extends AppCompatActivity implements View.OnClickListener
         arrows = (EditText)findViewById(R.id.arrowCountEdit);
         venue = (EditText) findViewById(R.id.venueEdit);
         details = (EditText)findViewById(R.id.journalDetailsEdit);
-        update = (Button)findViewById(R.id.buttonEditSave);
-        cancel = (Button)findViewById(R.id.buttonEditCancel);
 
-        update.setOnClickListener(this);
-        cancel.setOnClickListener(this);
 
         arrows.setText(c.getString(1));
         venue.setText(c.getString(3));
         details.setText(c.getString(4));
     }
 
-    public void onClick(View v){
-        if(v.getId() == update.getId()){
-            db.open();
-            db.updateEntry(b.getInt("id"),Integer.parseInt(arrows.getText().toString())
-                    ,c.getString(2), venue.getText().toString(), details.getText().toString());
-            db.close();
-            finish();
-        }
-        if(v.getId() == cancel.getId()){
-            finish();
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbaritems, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_save was selected
+            case R.id.action_save:
+                db.open();
+                db.updateEntry(b.getInt("id"),Integer.parseInt(arrows.getText().toString())
+                        ,c.getString(2), venue.getText().toString(), details.getText().toString());
+                db.close();
+                finish();
+                break;
+            case R.id.action_cancel:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+
 }
--- app/src/main/java/ie/dit/msd_assignment/MainActivity.java
+++ app/src/main/java/ie/dit/msd_assignment/MainActivity.java
@@ -1,25 +1,56 @@
 package ie.dit.msd_assignment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Size;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
     EditText v_arrows;
     EditText v_venue;
     EditText v_details;
     JournalDBManager dbm;
     Calendar c;
+    Button image;
+    ImageView mImageView;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +59,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar) ;
         setSupportActionBar(myToolbar);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
+
+        }
+
 
         //assigning the edittext views to their corresponding elements
         v_arrows = (EditText)findViewById(R.id.arrowCount);
         v_venue = (EditText)findViewById(R.id.venue);
         v_details = (EditText)findViewById(R.id.journalDetails);
+        image = (Button)findViewById(R.id.imageButton);
+        image.setOnClickListener(this);
+        mImageView = (ImageView)findViewById(R.id.mImageView);
 
 
         //seting up db connection
@@ -79,9 +118,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
                 }
             // action with ID action_settings was selected
-            case R.id.action_settings:
-                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
-                break;
+
 
             case R.id.action_cancel:
                 finish();
@@ -94,5 +131,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageButton:
+                doCameraStuff();
+                break;
+            default:
+                break;
+        }
     }
+
+    public void doCameraStuff(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Toast.makeText(this, "DEBUGGING", Toast.LENGTH_SHORT).show();
+        //if (intent.resolveActivity(getPackageManager()) != null) {
+            Toast.makeText(this, "starting cam intent", Toast.LENGTH_SHORT).show();
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        //}
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Toast.makeText(this, "cam intent successful", Toast.LENGTH_SHORT).show();
+                Bundle extras = data.getExtras();
+                mImageView.setImageBitmap((Bitmap) extras.get("data"));
+            }
+        }
+    }
+
+
+
 }
